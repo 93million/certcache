61a6c7f0cacae33507efd98fae9a77e5c12a71e5
pommy <tm_git@93m.co>
2019-07-25 10:23:44 +0200

    simplified mkdirRecursive() helper fn

--- src/helpers/mkdirRecursive.js
+++ src/helpers/mkdirRecursive.js
@@ -4,17 +4,17 @@ const mkdir = util.promisify(fs.mkdir)
 const fileExists = require('./fileExists')
 
 module.exports = async (path) => {
-  const dirsArr = path.split('/').filter((item) => item !== '')
+  const dirsArr = path.split('/').filter((item) => (item !== ''))
   const searchPaths = dirsArr.map((undefined, i, dirs) => (
     `/${[...dirs].splice(0, i + 1).join('/')}`
   ))
-  const missingSearchPaths = (await Promise.all(
-    searchPaths.map(async (path) => (await fileExists(path)) ? false : path)
-  ))
-    .filter((path) => (path !== false))
 
-  return missingSearchPaths.reduce(
-    (acc, path, i) => acc.then(() => mkdir(path)),
+  return searchPaths.reduce(
+    async (acc, path) => (
+      (await fileExists(path) === false)
+        ? acc.then(() => mkdir(path))
+        : acc
+    ),
     Promise.resolve()
   )
 }
