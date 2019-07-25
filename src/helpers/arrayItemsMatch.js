26ad2729faa5d7d47a9efbbb9f31f06f08988ead
pommy <tm_git@93m.co>
2019-07-25 10:20:55 +0200

    made domain matching function more robust when domain name orders don't correlate

--- src/helpers/arrayItemsMatch.js
+++ src/helpers/arrayItemsMatch.js
@@ -1,3 +1,7 @@
-module.exports = (a1, a2) => (
-  a1.length === a2.length && a1.every((item, i) => a2[i] === item)
-)
+module.exports = (a1, a2) => {
+  return (
+    a1.length === a2.length &&
+    a1.every((item, i) => a2.includes(item)) &&
+    a2.every((item, i) => a1.includes(item))
+  )
+}
