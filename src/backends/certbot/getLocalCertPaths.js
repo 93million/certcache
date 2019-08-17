2939071e4e8362cf9bda23d00e19d3194842b204
pommy <tm_git@93m.co>
2019-08-17 14:54:54 +0100

    Fixed file path issues

--- src/backends/certbot/getLocalCertPaths.js
+++ src/backends/certbot/getLocalCertPaths.js
@@ -5,7 +5,7 @@ const fileExists = require('../../lib/helpers/fileExists')
 const path = require('path')
 
 module.exports = async () => {
-  const certDir = path.resolve(__dirname, '..', '..', '..', '..', 'certbot', 'config', 'live')
+  const certDir = path.resolve(__dirname, '..', '..', '..', 'certbot', 'config', 'live')
   const dirItems = await readdir(certDir).catch(() => [])
   const certPaths = dirItems.map((item) => `${certDir}/${item}/cert.pem`)
   const existsArr = await Promise.all(certPaths.map(fileExists))
--- src/config/defaults.js
+++ src/config/defaults.js
@@ -2,12 +2,27 @@ const path = require('path')
 
 module.exports.certcacheHost = 'localhost'
 module.exports.certcachePort = 4433
-module.exports.certcacheCertDir = path.resolve(__dirname, '/../../certs/')
+module.exports.certcacheCertDir = path.resolve(__dirname, '..', '..', 'certs')
 module.exports.certbotExec = 'certbot'
 module.exports.certbotConfigDir = path.resolve(
   __dirname,
-  '/../../certbot/config/'
+  '..',
+  '..',
+  'certbot',
+  'config'
 )
-module.exports.certbotLogsDir = path.resolve(__dirname, '/../../certbot/logs/')
-module.exports.certbotWorkDir = path.resolve(__dirname, '/../../certbot/work/')
+module.exports.certbotLogsDir = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  'certbot',
+  'logs'
+)
+module.exports.certbotWorkDir = path.resolve(
+  __dirname,
+  '..',
+  '..',
+  'certbot',
+  'work'
+)
 module.exports.certcacheTmpDir = '/tmp/certcache/'
