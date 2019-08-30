0a63de2305a316588be6ac26a614d23874dfc7e5
pommy <tm_git@93m.co>
2019-08-30 16:10:23 +0100

    Removed unused var

--- src/lib/classes/CertList.js
+++ src/lib/classes/CertList.js
@@ -2,7 +2,7 @@ const arrayItemsMatch = require('../helpers/arrayItemsMatch')
 
 class CertList extends Array {
   findCert (searchCommonName, searchAltNames, extras) {
-    return this.find(({ altNames, certPath, commonName, issuerCommonName }) => {
+    return this.find(({ altNames, commonName, issuerCommonName }) => {
       const isTest = (issuerCommonName.indexOf('Fake') !== -1)
 
       return (
