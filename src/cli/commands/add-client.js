3ed8bd59e9f68371939a6bbc2303663440263b76
pommy <tm_git@93m.co>
2019-09-08 18:51:23 +0100

    Created command 'certcache add-client [name]' to add a new client key

---
+++ src/cli/commands/add-client.js
@@ -0,0 +1,31 @@
+const childProcess = require('child_process')
+const path = require('path')
+const util = require('util')
+
+const execFile = util.promisify(childProcess.execFile)
+
+module.exports = {
+  cmd: 'add-client [name]',
+  desc: 'Create access keys to allow certcache clients to access certcache server',
+  builder: (yargs) => {
+    yargs.positional('name', {
+      describe: 'Name of the client key',
+      required: true
+    })
+    yargs.option('keydir', {
+      default: process.env.CAH_KEYS_DIR ||
+        path.resolve(__dirname, '..', '..', 'cahkeys')
+    })
+  },
+  handler: (argv) => {
+    const execScript = 'client-authenticated-https'
+
+    execFile(
+      execScript,
+      ['create-key', '--keydir', argv.keydir, '--name', argv.name]
+    )
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+}
