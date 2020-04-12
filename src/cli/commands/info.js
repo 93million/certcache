918b25b736872ffdd649eee728201291ea254367
pommy <tm_git@93m.co>
2020-04-12 19:50:20 +0100

    Fixed help info and async handler for cli command 'info'

--- src/cli/commands/info.js
+++ src/cli/commands/info.js
@@ -2,8 +2,6 @@ const outputInfo = require('../../lib/client/outputInfo')
 
 module.exports = {
   cmd: 'info',
-  desc: 'Test the connection to the Certcache server',
-  handler: () => {
-    outputInfo()
-  }
+  desc: 'Display info about Certcache client and server',
+  handler: outputInfo
 }
