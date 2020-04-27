f3bf413894f3f46c02a7a90803cd572aff23363a
pommy <tm_git@93m.co>
2020-04-27 12:11:24 +0100

    Bugfix: requesting cert that doesn't exist in upstream cache throws TypeError when it tries to generate it using thridparty extension

--- src/lib/generateFirstCertInSequence.js
+++ src/lib/generateFirstCertInSequence.js
@@ -1,18 +1,20 @@
 const Certificate = require('./classes/Certificate')
 
 module.exports = async (certGenerators, commonName, altNames, meta) => {
-  return certGenerators.reduce(
-    async (acc, certGenerator) => (
-      (await acc) ||
-      Certificate.fromPath(
-        certGenerator,
-        await certGenerator.generateCert(
-          commonName,
-          altNames,
-          meta[certGenerator.id] || {}
+  return certGenerators
+    .filter((certGenerator) => (certGenerator.generateCert !== undefined))
+    .reduce(
+      async (acc, certGenerator) => (
+        (await acc) ||
+        Certificate.fromPath(
+          certGenerator,
+          await certGenerator.generateCert(
+            commonName,
+            altNames,
+            meta[certGenerator.id] || {}
+          )
         )
-      )
-    ),
-    Promise.resolve()
-  )
+      ),
+      Promise.resolve()
+    )
 }
