3578c155456c525dd48bc7159666122678ab8edf
pommy <tm_git@93m.co>
2020-05-16 12:42:15 +0100

    Unit tests for getExtensions()

--- src/lib/getExtensions.js
+++ src/lib/getExtensions.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const { promisify } = require('util')
+const requireModule = require('./helpers/requireModule')
 
 const readdir = promisify(fs.readdir)
 const stat = promisify(fs.stat)
@@ -8,12 +9,15 @@ const extensionsDir = path.resolve(__dirname, '..', 'extensions')
 
 let extensions
 
-module.exports = async () => {
-  if (extensions === undefined) {
+module.exports = async ({ noCache } = {}) => {
+  if (extensions === undefined || noCache === true) {
     extensions = (await readdir(extensionsDir)).reduce(
       async (acc, filename) => {
         if ((await stat(path.resolve(extensionsDir, filename))).isDirectory()) {
-          const extension = require(path.resolve(extensionsDir, filename));
+          const extension = requireModule(path.resolve(
+            extensionsDir,
+            filename
+          ));
 
           (await acc)[filename] = { ...extension, id: filename }
         }
---
+++ src/lib/getExtensions.test.js
@@ -0,0 +1,79 @@
+/* global jest test expect */
+
+const fs = require('fs')
+const path = require('path')
+const getExtensions = require('./getExtensions')
+const requireModule = require('./helpers/requireModule')
+
+jest.mock('fs')
+jest.mock('./helpers/requireModule')
+
+const mockDirContents = ['ext1', 'ext2', 'fileToIgnore']
+const ext1 = { foo: 'baa' }
+const ext2 = { baa: 'fee' }
+const mockExtensions = { ext1, ext2 }
+const extensionsDir = path.resolve(__dirname, '..', 'extensions')
+
+requireModule.mockImplementation((_path) => {
+  const key = Object.keys(mockExtensions).find((key) => {
+    return (_path === path.resolve(extensionsDir, key))
+  })
+
+  return key && mockExtensions[key]
+})
+
+// jest.mock(path.resolve(extensionsDir, 'ext1'))
+// jest.mock(path.resolve(extensionsDir, 'ext2'))
+
+fs.readdir.mockImplementation((path, callback) => {
+  callback(null, mockDirContents)
+})
+
+fs.stat.mockImplementation(async (_path, callback) => {
+  callback(
+    null,
+    {
+      isDirectory: () => {
+        return (path.basename(_path).startsWith('file') === false)
+      }
+    }
+  )
+})
+
+test(
+  'should generate list of extensions from directory contents',
+  async () => {
+    await expect(getExtensions()).resolves.toEqual({
+      ext1: { ...ext1, id: expect.any(String) },
+      ext2: { ...ext2, id: expect.any(String) }
+    })
+  }
+)
+
+test(
+  'should add generated id inside extension object',
+  async () => {
+    await expect(getExtensions()).resolves.toEqual({
+      ext1: { ...ext1, id: 'ext1' },
+      ext2: { ...ext2, id: 'ext2' }
+    })
+  }
+)
+
+test(
+  'should only include items that are directories',
+  async () => {
+    await expect(getExtensions()).resolves.not.toHaveProperty('fileToIgnore')
+  }
+)
+
+test(
+  'should cache results',
+  async () => {
+    await getExtensions({ noCache: true })
+    await getExtensions()
+
+    expect(requireModule)
+      .toHaveBeenCalledTimes(Object.keys(mockExtensions).length)
+  }
+)
