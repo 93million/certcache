1c0fdbb02914c7e45afe85aea7a9153be5a7e50e
pommy <tm_git@93m.co>
2020-05-19 19:21:16 +0100

    Bugfix: running cli command 'sync' and 'client' fail when env var 'CERTCACHE_HTTP_REDIRECT_URL' is present * Running cli 'sync' when env var 'CERTCACHE_HTTP_REDIRECT_URL' is present causes process to hang and never complete * Running cli 'client' when env var 'CERTCACHE_HTTP_REDIRECT_URL' is present throws error - unable to bind to port 80

--- src/lib/client/syncPeriodically.js
+++ src/lib/client/syncPeriodically.js
@@ -3,11 +3,14 @@ const syncCerts = require('../../lib/client/syncCerts')
 const httpRedirect = require('../httpRedirect')
 const setTimeoutPromise = require('../helpers/setTimeoutPromise')
 
+let httpRedirectLaunched
+
 const syncPeriodically = async (forever) => {
   const config = (await getConfig())
 
-  if (config.httpRedirectUrl !== undefined) {
+  if (config.httpRedirectUrl !== undefined && httpRedirectLaunched !== true) {
     httpRedirect.start(config.httpRedirectUrl)
+    httpRedirectLaunched = true
   }
 
   await syncCerts()
@@ -24,6 +27,9 @@ const syncPeriodically = async (forever) => {
       () => syncPeriodically(forever),
       1000 * config.syncInterval * 60
     )
+  } else if (config.httpRedirectUrl !== undefined) {
+    httpRedirect.stop()
+    httpRedirectLaunched = false
   }
 }
 
