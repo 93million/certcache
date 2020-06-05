6da848435ca4e81e4ebcda018fbc5e909eab4bbc
pommy <tm_git@93m.co>
2020-06-05 13:08:25 +0100

    Added --host and --cahkeys args to cli commands 'info'  and 'test'

--- src/cli/commands/info.js
+++ src/cli/commands/info.js
@@ -1,7 +1,9 @@
 const outputInfo = require('../../lib/client/outputInfo')
+const { cahkeys, host } = require('./args')
 
 module.exports = {
   cmd: 'info',
   desc: 'Display info about Certcache client and server',
+  builder: { cahkeys, host },
   handler: outputInfo
 }
--- src/cli/commands/testCmd.js
+++ src/cli/commands/testCmd.js
@@ -1,7 +1,9 @@
 const test = require('../../lib/client/testCmd')
+const { cahkeys, host } = require('./args')
 
 module.exports = {
   cmd: 'test',
   desc: 'Test the connection to the Certcache server',
+  builder: { cahkeys, host },
   handler: test
 }
