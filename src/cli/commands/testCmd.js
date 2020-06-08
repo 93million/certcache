993bc0ac088d2357b60d5f692cddf5ca8ad3192e
pommy <tm_git@93m.co>
2020-06-08 15:32:08 +0100

    Renamed cli arg '--host' -> '--upstream' for consistency with config and env vars

--- src/cli/commands/testCmd.js
+++ src/cli/commands/testCmd.js
@@ -1,9 +1,9 @@
 const test = require('../../lib/client/testCmd')
-const { cahkeys, host } = require('./args')
+const { cahkeys, upstream } = require('./args')
 
 module.exports = {
   cmd: 'test',
   desc: 'Test the connection to the Certcache server',
-  builder: { cahkeys, host },
+  builder: { cahkeys, upstream },
   handler: test
 }
